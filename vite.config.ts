c8cbdce394cd7e0a8d08c40d89baa24d27a4530a
HilaryGH <hilarygebremedhn28@gmail.com>
2025-06-07 22:42:41 +0300

    Fix base path in vite.config.js for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Hamlin-Crowdfunding/',
+ base: '/',
   plugins: [react(), tailwindcss(), svgr()],
 })
